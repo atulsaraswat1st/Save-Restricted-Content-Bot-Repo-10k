c3e3fced7f77e1297ab4393154feff759e5caddc
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-05-14 14:37:54 +0530

    Create mongo.py

---
+++ main/plugins/mongo.py
@@ -0,0 +1,30 @@
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+MONGO_DB = ""
+
+mongo = MongoCli(MONGO_DB)
+db = mongo.captions
+db = db.caption_db
+
+
+
+async def del_caption(user_id, replaced_word):
+    data = await check_premium(user_id)
+    if data and data.get("_id"):
+        await db.update_one({"_id": user_id}, {"$set": {"replaced_word": replaced_word}})
+    else:
+        await db.insert_one({"_id": user_id, "replaced_word": replaced_word})
+
+
+async def del_caption(user_id, del_word):
+    data = await check_premium(user_id)
+    if data and data.get("_id"):
+        await db.update_one({"_id": user_id}, {"$set": {"del_word": del_word}})
+    else:
+        await db.insert_one({"_id": user_id, "del_word": del_word})
+
+
+async def check_premium(user_id):
+    x = await db.find_one({"_id": user_id})
+    return x
+
