4df58a41e84baf763da7432132e22362dd4c5234
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-05-14 22:27:13 +0530

    Update replaced.py

--- main/plugins/replaced.py
+++ main/plugins/replaced.py
@@ -3,11 +3,13 @@ from main import Bot as app
 from pyromod import listen
 from main.plugins import mongo
 
+
+
 @app.on_message(filters.command("replaced"))
 async def replaced_word(_,message):
  lol = await _.ask(message.chat.id, text="send me a replaced word")
  replaced_txt = lol.text
- await mongo.add_replaced(message.from_user.id, replaced_txt)
+ await mongo.re_caption(message.from_user.id, replaced_txt)
  await message.reply_text("succeffully added your replaced word.")
  
 
@@ -15,6 +17,6 @@ async def replaced_word(_,message):
 async def delete_word(_,message):
  lol = await _.ask(message.chat.id, text="send me a delete word")
  del_txt = lol.text
- await mongo.add_replaced(message.from_user.id, del_txt)
+ await mongo.del_caption(message.from_user.id, del_txt)
  await message.reply_text("succeffully added your delete word.")
  
