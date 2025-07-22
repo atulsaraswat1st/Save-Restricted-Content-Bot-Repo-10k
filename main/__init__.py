3ae21fcf0cbee0157aafdc68cee7e64478c294b1
atulsaraswat1st <115392344+atulsaraswat1st@users.noreply.github.com>
2025-07-22 20:04:32 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -5,7 +5,6 @@ from pyromod import listen
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-from decouple import config
 import logging, time, sys
 
 logging.basicConfig(level=logging.DEBUG,
@@ -15,12 +14,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID ="15578503"
-API_HASH ="de14eccfa6fa8d7c2eee9656cc2bdc69"
-BOT_TOKEN ="7501679052:AAFhMm4dtaXIcRiY4tBXn54tguj1H9CzCeg"
-SESSION ="BQDttYcAB7XpUoi-dY4m_5YPbcyoJm5L51BIjIWQH2IMAYVnzTtytKU3513SZncor5-SrCf-VqLAsN5J6SoT0L4NSlwqTNEkoPxkaKqHCgYUjnPkIAZY8O2jGSssrzWwTyBt8EQMzT53kTfNQxieGra6AIhPGgqUgZcw6t5p0BVf4YJxM32MljPOqOjjFLImzBvMMq1rSnUbxKvHhTvYhQnGKj5MFgOjpHU2MUEaQiJLvoxg4T97nDO-bxYa4DzZZ3zXCQVcAPSegk3T3zevrHI79DcWdcoVt-Kw-kv2EQ_0HCyo9hT6Yp-2jKrziD9YonlaZLok7xWGfrto3kHbZqtFXPMIvQAAAAFJWkbdAA"
-FORCESUB ="AtulChannelhainbhai"
-AUTH ="5525620445"
+API_ID = "15663518"
+API_HASH = "d675b3e5ad6fcf248d6ddafc8c07c53b"
+BOT_TOKEN = "8163129853:AAG3RimnlpjWdD9FHdXEM_gk_sL3T51rvrc"
+SESSION = "BQDvAZ4AUPk2WgGR9V-R7k00wqR2d_4FKV3HCaZM_ZLF9OhfB0KM4KrKfWsDTtU4VDAiCfOobY5-8qLiuXYBIM1AV2C7SetUg4kImLOfzYkboxAWToS41hLSmrSzJhKAxf3jSIPx4WIZqFWabknxTV8gNelVjIeMlok_VyKf3X8AM5GkmiGDJGdH6ZbXSR5dVr9cZmv1utCyxjDm7Nuu21W21IbSK0eMSxgp0owkhSFSWbjN6CWBAoQiiaDuGapaWLMsKpuute4y3KlvUtxBbaMtn2mEKRHJjH0s2q-vL8JqtagT1h4CTsYPHTqVt3C5jyYDd1G4bchEXh_dfk8M_Yw90whECgAAAAGqNXUWAA"
+FORCESUB = "srcbopbothain"
+AUTH = "7150597398"
 SUDO_USERS = []
 
 if len(AUTH) != 0:
@@ -34,12 +33,12 @@ bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 #    session_name=SESSION, 
 #    api_hash=API_HASH, 
 #    api_id=API_ID)
-userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
+userbot = Client("myacc", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
 
 try:
     userbot.start()
 except BaseException:
-    print("Your session expired please re add that... thanks @dev_gagan.")
+    print("Your session expired please re add that... thanks @scrpbothainbhai.")
     sys.exit(1)
 
 Bot = Client(
